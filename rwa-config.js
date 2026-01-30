f864b766fe82c602f1aa225cef5252be5f3d47ac
Parmita <160781867+0xparomita@users.noreply.github.com>
2026-01-30 14:10:15 +0600

    Create rwa-config.js

---
+++ rwa-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN_NAME: "Tokenized NYC Real Estate",
+    TOKEN_SYMBOL: "NYCRE",
+    IDENTITY_REGISTRY_ADDRESS: "0x...", // Address of your KYC provider's contract
+    INITIAL_MINT: "1000000"
+};
